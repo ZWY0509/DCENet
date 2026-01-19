74011164b2a0d7454284c99b3339cb104ee284d2
ZWYzwy <1047234871@qq.com>
2026-01-19 14:13:52 +0800

    Update summary.py

--- basicsr/archs/summary.py
+++ basicsr/archs/summary.py
@@ -1,19 +1,1 @@
-from thop import profile
-import torch
-from archs.LDMN_arch import LDMN as net
-# from archs.MSGAN import MSGAN as net
-# from archs.MSGAN2_arch import MSGAN2 as net
-#from archs.RCAN import Ablation as netpipWDWW
-model = net()
-input = torch.randn(1, 3, 640, 360)         # x2
-# input = torch.randn(1, 3, 420, 240)         # x3
-#input = torch.randn(1, 3, 320, 180)         # x4
-
-macs, params = profile(model, inputs=(input, ))
-
-print("Multi-adds[G] ")
-print(macs/1e9)
-print("Parameters [K]")
-print(params/1e3)
-
 
